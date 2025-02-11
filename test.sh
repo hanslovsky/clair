f9fc3567f5e78fcd7be570e80761c4f6ec3576b4
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2025-02-10 21:09:27 -0500

    Add shell test and github actions

---
+++ test.sh
@@ -0,0 +1,13 @@
+function _myshuf() {
+    shuf --random-source=Cargo.toml -i 1-$1
+}
+
+exit_code=0
+
+diff -aruN <(wc -l Cargo.toml | sed 's/ .*//') <(./target/release/count < Cargo.toml) || exit_code=1
+diff -aruN <(_myshuf 100 | awk '{x+=$1} END {print(x/100)}') <(_myshuf 100 | ./target/release/mean) || exit_code=1
+diff -aruN <(_myshuf 6 | awk 'BEGIN {x=1}{x*=$1} END {print(x)}') <(_myshuf 6 | ./target/release/product) || exit_code=1
+diff -aruN <(_myshuf 100 | awk '{x+=$1} END {print(x)}') <(_myshuf 100 | ./target/release/sum) || exit_code=1
+
+exit_code=1  # Add this to fail on purpose to check this in the beginning
+exit $exit_code
